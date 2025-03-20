c41c19a1f3e4cd83fc9cd7dc3dfcea4f1c299760
Oshawhat <alannobledall@gmail.com>
2025-03-20 01:45:06 -0400

    add all the foods and recipes

--- recipes.gd
+++ recipes.gd
@@ -8,7 +8,12 @@ enum actions {
 	AUTO,
 }
 
-#	[ FoodItemName1, FoodItemName2, ... ]: [ Action, ResultItem ]
+#	[ "FoodItemName1", "FoodItemName2", ... ]:		[ actions.Action, preload("res://ResultItem.tscn") ],
 const recipes = {
-	[ "Lettuce", "Tomato", "CookedChicken" ]: [ actions.MIX, preload("res://Scenes/Item/FoodItem/chicken_salad.tscn") ],
+	[ "Flour", "CookedChicken" ]:		[ actions.COOK, preload("res://Scenes/Item/FoodItem/fried_chicken.tscn") ],
+	[ "Lettuce", "CookedChicken" ]:		[ actions.MIX, preload("res://Scenes/Item/FoodItem/chicken_salad.tscn") ],
+	[ "Dough", "TomatoPaste", "GratedCheese" ]:		[ actions.COOK, preload("res://Scenes/Item/FoodItem/pizza.tscn") ],
+	[ "Pasta", "TomatoPaste", "GratedCheese" ]:		[ actions.MIX, preload("res://Scenes/Item/FoodItem/spaghetti.tscn") ],
+	[ "Bread", "CookedChicken", "Cheese", "Lettuce" ]:		[ actions.MIX, preload("res://Scenes/Item/FoodItem/chicken_sandwich.tscn") ],
+	[ "Pasta", "CookedChicken", "TomatoPaste", "Lettuce", "GratedCheese" ]:		[ actions.MIX, preload("res://Scenes/Item/FoodItem/tuscan_chicken_pasta.tscn") ],
 }
