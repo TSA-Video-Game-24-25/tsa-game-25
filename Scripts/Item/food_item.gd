b8a8ef702309a047ecc7c2ffd2761f4a23335799
Oshawhaat <alannobledall@gmail.com>
2025-03-20 22:02:09 -0400

    create stove

--- Scripts/Item/food_item.gd
+++ Scripts/Item/food_item.gd
@@ -5,5 +5,6 @@ class_name FoodItem
 @export var MixResult: PackedScene
 @export var CutResult: PackedScene
 @export var CookResult: PackedScene
+@export var StoveResult: PackedScene
 @export var CookTime: int = 5
 @export var IsDone := false
