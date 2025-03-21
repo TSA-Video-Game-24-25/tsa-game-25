9179be080ceda815f9fd92d8471076f529587f1f
Oshawhat <alannobledall@gmail.com>
2025-03-21 13:19:24 -0400

    adjust ui

--- Scenes/GUI/ui.gd
+++ Scenes/GUI/ui.gd
@@ -1,9 +1,18 @@
 extends Control
 
+
 @onready var main: Main = get_tree().get_root().get_node("Main")
+
+
+func _process(delta: float) -> void:
+	$CanvasLayer/Panel/HBoxContainer/Panel/DayLabel.text = "Day " + str(main.day_num)
+	$CanvasLayer/Panel/HBoxContainer/Panel2/TimeLabel.text = str(snapped(main.time_remaining, .1))
+	$CanvasLayer/Panel/HBoxContainer/Panel3/ScoreLabel.text = "Score: " + str(main.score)
+
 
 func startDay() -> void:
 	return
+
 
 func endDay() -> void:
 	return
