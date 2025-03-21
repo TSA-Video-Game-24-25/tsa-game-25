12ebaf50714a69301494caf11616fb33941df3e6
Oshawhaat <alannobledall@gmail.com>
2025-03-20 22:16:22 -0400

    update recipes

--- recipes.gd
+++ recipes.gd
@@ -5,6 +5,7 @@ enum actions {
 	CUT,
 	MIX,
 	COOK,
+	STOVE,
 	AUTO,
 }
 
@@ -13,7 +14,7 @@ const recipes = {
 	[ "Flour", "CookedChicken" ]:		[ actions.COOK, preload("res://Scenes/Item/FoodItem/fried_chicken.tscn") ],
 	[ "Lettuce", "CookedChicken" ]:		[ actions.MIX, preload("res://Scenes/Item/FoodItem/chicken_salad.tscn") ],
 	[ "Dough", "TomatoPaste", "GratedCheese" ]:		[ actions.COOK, preload("res://Scenes/Item/FoodItem/pizza.tscn") ],
-	[ "Pasta", "TomatoPaste", "GratedCheese" ]:		[ actions.COOK, preload("res://Scenes/Item/FoodItem/spaghetti.tscn") ],
+	[ "Pasta", "TomatoPaste", "GratedCheese" ]:		[ actions.STOVE, preload("res://Scenes/Item/FoodItem/spaghetti.tscn") ],
 	[ "Bread", "CookedChicken", "Cheese", "Lettuce" ]:		[ actions.AUTO, preload("res://Scenes/Item/FoodItem/chicken_sandwich.tscn") ],
-	[ "Pasta", "CookedChicken", "TomatoPaste", "Lettuce", "GratedCheese" ]:		[ actions.COOK, preload("res://Scenes/Item/FoodItem/tuscan_chicken_pasta.tscn") ],
+	[ "Pasta", "CookedChicken", "TomatoPaste", "Lettuce", "GratedCheese" ]:		[ actions.STOVE, preload("res://Scenes/Item/FoodItem/tuscan_chicken_pasta.tscn") ],
 }
