1b3a7720831da18d9b4ddf98a44356a8cc9feb75
Oshawhat <alannobledall@gmail.com>
2025-03-22 15:57:39 -0500

    Make UI look a bit nicer

--- Scenes/GUI/ui.gd
+++ Scenes/GUI/ui.gd
@@ -7,7 +7,7 @@ class_name UI
 @onready var DayEnd = $DayEnd
 
 const DayStartStr = "Day %d\n%d Total Pts"
-const DayEndStr = "Day %d\n%d Earned Pts\n%d Total Pts"
+const DayEndStr = "Day %d\n%d Earned Pts\n%d Total Pts\n"
 
 
 func _process(delta: float) -> void:
@@ -15,26 +15,28 @@ func _process(delta: float) -> void:
 	$CanvasLayer/TopPanel/HBoxContainer/Panel2/TimeLabel.text = str(snapped(main.time_remaining, .1))
 	$CanvasLayer/TopPanel/HBoxContainer/Panel3/ScoreLabel.text = "Score: " + str(main.score)
 	
-	$CanvasLayer/BottomPanel/HBoxContainer/Player1/GrabLabel1.visible = main.Players[0].can_grab()
-	$CanvasLayer/BottomPanel/HBoxContainer/Player1/UseLabel1.visible = main.Players[0].can_use()
+	$CanvasLayer/BottomPanel/HBoxContainer/Player1/VBoxContainer/GrabLabel1.visible = main.Players[0].can_grab()
+	$CanvasLayer/BottomPanel/HBoxContainer/Player1/VBoxContainer/UseLabel1.visible = main.Players[0].can_use()
 	
-	$CanvasLayer/BottomPanel/HBoxContainer/Player2/GrabLabel2.visible = main.Players[1].can_grab()
-	$CanvasLayer/BottomPanel/HBoxContainer/Player2/UseLabel2.visible = main.Players[1].can_use()
+	$CanvasLayer/BottomPanel/HBoxContainer/Player2/VBoxContainer/GrabLabel2.visible = main.Players[1].can_grab()
+	$CanvasLayer/BottomPanel/HBoxContainer/Player2/VBoxContainer/UseLabel2.visible = main.Players[1].can_use()
 
 
 func startDay() -> void:
-	DayStart.find_child("Label").text = DayStartStr % [main.day_num, main.total_score]
-	DayStart.visible = true
+	$DayStart/PanelContainer/Label.text = DayStartStr % [main.day_num, main.total_score]
+	$DayStart.visible = true
+	
 	await get_tree().create_timer(2.0).timeout
 	DayStart.visible = false
 	return
 
 
 func endDay() -> void:
-	DayEnd.find_child("Label").text = DayEndStr % [main.day_num, main.score, main.total_score]
-	DayEnd.visible = true
-	var dayEndBtn:Button = DayEnd.find_child("Button")
+	$DayEnd/PanelContainer/VBoxContainer/Label.text = DayEndStr % [main.day_num, main.score, main.total_score]
+	$DayEnd.visible = true
+	
+	var dayEndBtn: Button = $DayEnd/PanelContainer/VBoxContainer/Button
 	await dayEndBtn.pressed
-	DayEnd.visible = false
+	$DayEnd.visible = false
 	main.start_day()
 	return
