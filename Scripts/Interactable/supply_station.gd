059149906a613898823804bd948165c9e9a4c3db
Oshawhat <alannobledall@gmail.com>
2025-03-26 10:13:30 -0500

    fix plates with items being able to be returned to plate supply station

--- Scripts/Interactable/supply_station.gd
+++ Scripts/Interactable/supply_station.gd
@@ -9,6 +9,10 @@ func interact(player: Player):
 	if not player.heldItem:
 		player.tryAddItemFromScene(SuppliedItem)
 		return
+	
+	if player.heldItem is Plate and player.heldItem.heldItems:
+		return
+	
 	if player.heldItem.name == SuppliedItem.instantiate().name:
 		player.heldItem.queue_free()
 		player.heldItem = null
