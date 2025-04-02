29b58ad3eb7c8d7824c6553a8a86e494a9e272b7
Oshawhat <alannobledall@gmail.com>
2025-04-02 08:45:31 -0500

    fix day/score/time display covering counters

--- Scripts/GUI/ui.gd
+++ Scripts/GUI/ui.gd
@@ -28,7 +28,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	$Overlay/TopPanel/HBoxContainer/Panel/DayLabel.text = "Day " + str(main.day_num)
-	$Overlay/TopPanel/HBoxContainer/Panel2/TimeLabel.text = str(snapped(main.time_remaining, .1))
+	$Overlay/TopPanel/HBoxContainer/Panel2/VBoxContainer/TimeLabel.text = str(snapped(main.time_remaining, .1))
 	$Overlay/TopPanel/HBoxContainer/Panel3/ScoreLabel.text = "Score: " + str(main.score)
 	
 	$Overlay/BottomPanel/HBoxContainer/Player1/VBoxContainer/GrabLabel1.visible = main.Players[0].can_grab()
