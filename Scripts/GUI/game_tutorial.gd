cd9fedb60db7c1797e9c632171ccbafce9eede89
Oshawhat <alannobledall@gmail.com>
2025-04-03 10:16:00 -0500

    add update goal func

--- Scripts/GUI/game_tutorial.gd
+++ Scripts/GUI/game_tutorial.gd
@@ -49,130 +49,184 @@ func process_signal(parameter:=""):
 		action_complete.emit()
 
 
+func update_goal(new_text := "", new_spriteframes: SpriteFrames = null, new_frame: int = 1):
+	label.text = new_text
+	sprite.sprite_frames = new_spriteframes
+	sprite.frame = new_frame
+
+
 func start_tutorial():
 	visible = true
 	
-	label.text = get_order
-	sprite.sprite_frames = load("res://Art/Spriteframes/OrderCounter.tres")
+	update_goal(
+		get_order,
+		load("res://Art/Spriteframes/OrderCounter.tres")
+	)
 	
 	await main.order_taken
 	
 	await salad_tutorial()
 	
-	label.text = get_order
-	sprite.sprite_frames = load("res://Art/Spriteframes/OrderCounter.tres")
+	update_goal(
+		get_order,
+		load("res://Art/Spriteframes/OrderCounter.tres")
+	)
 	
 	await main.order_taken
 	
 	await fried_chicken_tutorial()
 	
-	label.text = tutorial_complete
-	sprite.sprite_frames = null
+	update_goal(tutorial_complete)
 
 
 func salad_tutorial():
-	label.text = grab_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/PlateStation.tres")
+	update_goal(
+		grab_plate,
+		load("res://Art/Spriteframes/PlateStation.tres")
+	)
 	
 	await action(main.item_interact, "grab_Plate")
 	
-	label.text = place_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/Counter.tres")
+	update_goal(
+		place_plate,
+		load("res://Art/Spriteframes/Counter.tres")
+	)
 	
 	await action(main.item_interact, "place_Plate")
 	
-	label.text = grab_lettuce
-	sprite.sprite_frames = load("res://Art/Spriteframes/LettuceStation.tres")
+	update_goal(
+		grab_lettuce,
+		load("res://Art/Spriteframes/LettuceStation.tres")
+	)
 	
 	await action(main.item_interact, "grab_Lettuce")
 	
-	label.text = place_on_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/Plate.tres")
+	update_goal(
+		place_on_plate,
+		load("res://Art/Spriteframes/Plate.tres")
+	)
 	
 	await action(main.item_interact, "place_Lettuce_plate")
 	
-	label.text = grab_chicken
-	sprite.sprite_frames = load("res://Art/Spriteframes/ChickenStation.tres")
+	update_goal(
+		grab_chicken,
+		load("res://Art/Spriteframes/ChickenStation.tres")
+	)
 	
 	await action(main.item_interact, "grab_Chicken")
 	
-	label.text = oven_ingredient
-	sprite.sprite_frames = load("res://Art/Spriteframes/Oven.tres")
+	update_goal(
+		oven_ingredient,
+		load("res://Art/Spriteframes/Oven.tres")
+	)
 	
 	await action(main.item_interact, "place_Chicken_Oven")
 	
-	label.text = wait_for_cook
-	sprite.frame = 2
+	update_goal(
+		wait_for_cook,
+		load("res://Art/Spriteframes/Oven.tres"),
+		2
+	)
 	
 	await action(main.item_interact, "grab_CookedChicken")
 	
-	label.text = place_on_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/Plate.tres")
+	update_goal(
+		place_on_plate,
+		load("res://Art/Spriteframes/Plate.tres")
+	)
 	
 	await action(main.item_interact, "place_CookedChicken_plate")
 	
-	label.text = mix_dish
-	sprite.sprite_frames = load("res://Art/Spriteframes/Plate.tres")
+	update_goal(
+		mix_dish,
+		load("res://Art/Spriteframes/Plate.tres")
+	)
 	
 	await action(main.item_processed, "ChickenSalad")
 	
-	label.text = order_complete
-	sprite.sprite_frames = load("res://Art/Spriteframes/PickupCounter.tres")
+	update_goal(
+		order_complete,
+		load("res://Art/Spriteframes/PickupCounter.tres")
+	)
 	
 	await action(main.item_interact, "place_ChickenSalad_DepositStation")
 	
-	label.text = finish_order
+	update_goal(
+		finish_order,
+		load("res://Art/Spriteframes/PickupCounter.tres")
+	)
 	
 	await action(main.item_process, "ChickenSalad_DepositStation")
 
 
 func fried_chicken_tutorial():
-	label.text = grab_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/PlateStation.tres")
+	update_goal(
+		grab_plate,
+		load("res://Art/Spriteframes/PlateStation.tres")
+	)
 	
 	await action(main.item_interact, "grab_Plate")
 	
-	label.text = place_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/Counter.tres")
+	update_goal(
+		place_plate,
+		load("res://Art/Spriteframes/Counter.tres")
+	)
 	
 	await action(main.item_interact, "place_Plate")
 	
-	label.text = grab_chicken
-	sprite.sprite_frames = load("res://Art/Spriteframes/ChickenStation.tres")
+	update_goal(
+		grab_chicken,
+		load("res://Art/Spriteframes/ChickenStation.tres")
+	)
 	
 	await action(main.item_interact, "grab_Chicken")
 	
-	label.text = place_on_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/Plate.tres")
+	update_goal(
+		place_on_plate,
+		load("res://Art/Spriteframes/Plate.tres")
+	)
 	
 	await action(main.item_interact, "place_Chicken_plate")
 	
-	label.text = grab_flour
-	sprite.sprite_frames = load("res://Art/Spriteframes/FlourStation.tres")
+	update_goal(
+		grab_flour,
+		load("res://Art/Spriteframes/FlourStation.tres")
+	)
 	
 	await action(main.item_interact, "grab_Flour")
 	
-	label.text = place_on_plate
-	sprite.sprite_frames = load("res://Art/Spriteframes/Plate.tres")
+	update_goal(
+		place_on_plate,
+		load("res://Art/Spriteframes/Plate.tres")
+	)
 	
 	await action(main.item_interact, "place_Flour_plate")
 	
-	label.text = stove_dish
-	sprite.sprite_frames = load("res://Art/Spriteframes/Stove.tres")
+	update_goal(
+		stove_dish,
+		load("res://Art/Spriteframes/Stove.tres")
+	)
 	
 	await action(main.item_interact, "place_Plate_Stove")
 	
-	label.text = wait_for_cook
-	sprite.sprite_frames = load("res://Art/Spriteframes/Stove.tres")
-	sprite.frame = 2
+	update_goal(
+		wait_for_cook,
+		load("res://Art/Spriteframes/Stove.tres"),
+		2
+	)
 	
 	await action(main.item_interact, "grab_FriedChicken")
 	
-	label.text = order_complete
-	sprite.sprite_frames = load("res://Art/Spriteframes/PickupCounter.tres")
+	update_goal(
+		order_complete,
+		load("res://Art/Spriteframes/PickupCounter.tres")
+	)
 	
 	await action(main.item_interact, "place_FriedChicken_DepositStation")
 	
-	label.text = finish_order
+	update_goal(
+		finish_order,
+		load("res://Art/Spriteframes/PickupCounter.tres")
+	)
 	
 	await action(main.item_process, "FriedChicken_DepositStation")
